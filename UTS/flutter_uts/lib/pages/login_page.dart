776d62f291e5ffd639c3bed774d73d9f1a619bc0
unknown <zidanahdi@gmail.com>
2024-10-22 14:31:44 +0700

    UTS PemMobile

---
+++ UTS/flutter_uts/lib/pages/login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/login_form.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'Login Page',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Center(
+          child: LoginForm(),
+        ),
+      ),
+    );
+  }
+}
